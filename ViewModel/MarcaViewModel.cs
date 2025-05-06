b13e135a3dca44cd029bec2007997e674a0542fb
Ghostcito <jmguticasani1254@gmail.com>
2025-05-06 00:04:17 -0500

    Desarrollo de View Model

---
+++ ViewModel/MarcaViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Soft_W_C.Models;
+
+namespace Soft_W_C.ViewModel
+{
+    public class MarcaViewModel
+    {
+        public Asistencia asistencia { get; set; }
+        public Usuario usuario { get; set; }
+    }
+}
