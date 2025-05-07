c0e032691254b6a4020ee463098e8e2a53fcbf71
Ghostcito <jmguticasani1254@gmail.com>
2025-05-07 09:01:34 -0500

    Relacion_Empleado_Supervisor

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -11,16 +11,20 @@ namespace Soft_W_C.Models
     //ya combinamos empleados y administrativos en un solo modelo
     public class Usuario : IdentityUser
     {
-    public string? Nombre { get; set; }
-    public string? Apellido { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
 
-    [Column(TypeName ="nvarchar(8)") ]
-    public string? DNI { get; set; }
-    public Sede? IdSede {get;set;}
-    public DateTime? FechaIngreso { get; set; } // fecha de ingreso a la empresa
-    public DateTime? FechaNacimiento { get; set; }
-    public string? NivelAcceso { get; set; }
-    public string? Estado { get; set; } //activo o inactivo
-    public decimal? Salario { get; set; }
+        [Column(TypeName = "nvarchar(8)")]
+        public string? DNI { get; set; }
+        public Sede? IdSede { get; set; }
+        public DateTime? FechaIngreso { get; set; } // fecha de ingreso a la empresa
+        public DateTime? FechaNacimiento { get; set; }
+        public string? NivelAcceso { get; set; }
+        public string? Estado { get; set; } //activo o inactivo
+        public decimal? Salario { get; set; }
+
+        //RELACIONES EMPLEADO - SUPERVISOR
+        public ICollection<Supervision> EmpleadosSupervisados { get; set; } // Si es Supervisor
+        public ICollection<Supervision> SupervisoresAsignados { get; set; } // Si es Empleado
     }
 }
