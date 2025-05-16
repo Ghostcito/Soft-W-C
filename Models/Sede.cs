0cfa094ca0e66891f9cb60ce58f80e942092fdde
leonardoPBF <leonardobf140224@gmail.com>
2025-05-16 13:21:36 -0500

    implementacion navegacion vistas controller

--- Models/Sede.cs
+++ Models/Sede.cs
@@ -20,11 +20,11 @@ namespace SoftWC.Models
         public Cliente Cliente { get; set; }
 
         //añadido para la relacion de usuario y sede
-        public string UsuarioId { get; set; }
+        public string? UsuarioId { get; set; }
         
         [ValidateNever]
         [ForeignKey("UsuarioId")]
-        public Usuario Usuario { get; set; }
+        public Usuario? Usuario { get; set; }
 
         public string? Nombre_local { get; set; } //para que puedan ubicar en caso la empresa tenga un local con nombre
         public string? Direccion_local { get; set; }
