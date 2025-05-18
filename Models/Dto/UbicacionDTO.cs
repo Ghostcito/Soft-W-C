ba313ebe1f22b0a9ed36f324d20e07c16ecfc319
Ghostcito <jmguticasani1254@gmail.com>
2025-05-18 18:45:35 -0500

    Objeto de Transferencia para Ubicacion

---
+++ Models/Dto/UbicacionDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftWC.Models.Dto
+{
+    public class UbicacionDTO
+    {
+        
+    }
+}
