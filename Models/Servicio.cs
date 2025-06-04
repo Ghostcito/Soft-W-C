e2a0a7d27c569e2de316bda2c1359796fa702b1f
leonardoPBF <leonardobf140224@gmail.com>
2025-06-04 16:44:33 -0500

    Implementacion de apartado servicio en dashboard administrador metodos crud

--- Models/Servicio.cs
+++ Models/Servicio.cs
@@ -14,18 +14,23 @@ namespace SoftWC.Models
 
     [Required]
     [StringLength(100)]
+    [Display(Name = "Nombre de servicio")]
     public string NombreServicio { get; set; }
 
     [Required]
+    [Display(Name = "Tipo de servicio")]
     public string TipoServicio { get; set; }
 
     [Required]
+    [Display(Name = "Descripción")]
     public string Descripcion { get; set; }
 
     [Column(TypeName = "decimal(10,2)")]
+    [Display(Name = "Precio base por hora de trabajo")]
     public decimal PrecioBase { get; set; }
 
     // Duración estimada del servicio
+    [Display(Name = "Duración estimada")]
     public TimeSpan? DuracionEstimada { get; set; }
 }
 }
